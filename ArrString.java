236b36919c9d70eadfaf73fd7c1b16323b24d3f2
Ashu24-code <chavanashwini869@gmail.com>
2025-01-31 20:09:15 +0530

    Add files via upload

---
+++ ArrString.java
@@ -0,0 +1,23 @@
+ public class ArrString {
+    public static void main(String[] args)  {
+         /*String [] arr = {"Chavan","Patil","Deshmukh","Jadhav","Shinde","Kadam"};
+       
+         for( int i=0;i< arr.length ;i++)
+         {
+             System.out.print( arr[i]);
+             System.out.print( " ");
+         }
+}
+    }**/
+    String[] arr=new String[5];
+        arr[0] = "Kadam";
+        arr[1] = "Chavan";
+        arr[2] = "Patil";
+        arr[3] = "shinde";
+        arr[4] = "Jadhav";
+       
+
+        System.out.println(arr[4]);
+}
+    }
+
